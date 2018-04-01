2fbdd2c797f73bfcdb3623c14dec66d56f2c810a
Momo Peng <yr.peng571@gmail.com>
2018-04-01 16:41:21 +0800

    update readme file

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
 # Core library
 
-最後修改日期 2018/03/29 MomoPeng
+最後修改日期 2018/03/31 MomoPeng
 
 用來幫助Android Application快速建立的Library。
 
  [ ![Download](https://api.bintray.com/packages/peng571/android_quick_build_tools/tool-core/images/download.svg?version=1.0.0) ](https://bintray.com/peng571/android_quick_build_tools/tool-core/1.0.0/link)
 
 
-## Gradle
+
+## Usage
+
+#### Gradle
 
 if useing gradle, add this line into apps build.gradle
 
-    compile 'org.pengyr.tool:tool-core:1.0.0'
+    implementation 'org.pengyr.tool:tool-core:1.0.0'
 
-## Maven
+#### Maven
 
-if useing maven
+or useing maven
 
-    <dependency> <groupId>org.pengyr.tool</groupId> <artifactId>tool-core</artifactId> <version>1.0.0</version> <type>pom</type> </dependency>
+    <dependency>
+        <groupId>org.pengyr.tool</groupId>
+        <artifactId>tool-core</artifactId>
+        <version>1.0.0</version>
+        <type>pom</type>
+    </dependency>
 
 ## TODO
 
+- 架構獨立，將多餘的元件另外開新的module
 - 完成所有文件說明。
 - 整理未使用的物件和library。
+
+
 
  
--- tool-core/src/main/java/org/pengyr/tool/core/tool/json/SimpleMapper.java
+++ tool-core/src/main/java/org/pengyr/tool/core/tool/json/SimpleMapper.java
@@ -1,9 +1,18 @@
 package org.pengyr.tool.core.tool.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.lang.reflect.Modifier;
+import org.pengyr.tool.core.log.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * Create simple method to use ObjectMapper of Jackson
@@ -11,37 +20,84 @@ import java.lang.reflect.Modifier;
  */
 public class SimpleMapper {
 
+
     private static final String TAG = SimpleMapper.class.getSimpleName();
 
-    private static Gson gson;
+    private static ObjectMapper mapper;
 
-    private static final String EMPTY_JSON_ARRAY = "[]";
-    private static final String EMPTY_JSON = "{}";
-
-    public static Gson getMapper() {
-        if (gson == null) {
-            gson = new GsonBuilder()
-                    //                    .excludeFieldsWithoutExposeAnnotation()
-                    .excludeFieldsWithModifiers(Modifier.STATIC | Modifier.FINAL)
-                    .setPrettyPrinting()
-                    .create();
+    public static ObjectMapper getMapper() {
+        if (mapper == null) {
+            mapper = new ObjectMapper();
         }
-        return gson;
+        return mapper;
     }
 
 
     // Object Wrapper
     public static String toString(Object object) {
-        if (object == null) return EMPTY_JSON;
-        return getMapper().toJson(object);
+
+        try {
+            return getMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            Logger.E(TAG, e);
+            return "";
+        }
     }
 
 
-    public static <T> T toObject(Class<T> type, String jsonString) {
-        return getMapper().fromJson(jsonString, type);
+    public static <T> T toObject(String jsonString, Class<T> c) {
+        try {
+            return getMapper().readValue(jsonString, c);
+        } catch (JsonParseException | JsonMappingException e1) {
+            Logger.E(TAG, e1);
+            return null;
+        } catch (IOException e2) {
+            Logger.E(TAG, e2);
+            return null;
+        }
     }
 
-    //    public static <T> List<T> toObjectList(Class<List<T>> type, String jsonString) {
-    //        return getMapper().fromJson(jsonString, type);
-    //    }
+    public static <T> List<T> toObjectList(InputStream jsonInput, Class<T> c) {
+        try {
+            return getMapper().readValue(jsonInput, getMapper().getTypeFactory().constructCollectionType(List.class, c));
+        } catch (JsonParseException | JsonMappingException e1) {
+            Logger.E(TAG, e1);
+            return null;
+        } catch (IOException e) {
+            Logger.E(TAG, e);
+            return null;
+        }
+    }
+
+    public static <T> List<T> toObjectList(String jsonString, Class<T> c) {
+        try {
+            return getMapper().readValue(jsonString, getMapper().getTypeFactory().constructCollectionType(List.class, c));
+        } catch (JsonParseException | JsonMappingException e1) {
+            Logger.E(TAG, e1);
+            return null;
+        } catch (IOException e) {
+            Logger.E(TAG, e);
+            return null;
+        }
+    }
+
+
+    public static <T> ArrayNode toArrayNode(T[] array) {
+        return mapper.valueToTree(array);
+    }
+
+
+    /**
+     * Simple node method to cover Org.JSON method
+     */
+    public static ObjectNode newJson() {
+        return JsonNodeFactory.instance.objectNode();
+    }
+
+    /**
+     * Simple node method to cover Org.JSONArray method
+     */
+    public static ArrayNode newArrayNode() {
+        return JsonNodeFactory.instance.arrayNode();
+    }
 }
